37d78e0511ffcb5c1dc8aa4cc8442c361380d7a1
andreaconv <andrea2convertini@gmail.com>
2023-09-19 15:02:24 +0200

    [create] classe Libro

---
+++ src/org/lessons/java/books/Libro.java
@@ -0,0 +1,58 @@
+package org.lessons.java.books;
+
+public class Libro {
+	
+	private String titolo;
+	private int numPagine;
+	private String autore;
+	private String editore;
+	
+	
+	public Libro(String titolo, int numPagine, String autore, String editore) {
+
+		setTitolo(titolo);
+		setNumPagine(numPagine);
+		setAutore(autore);
+		setEditore(editore);
+	}
+	
+	//TITOLO
+	public String getTitolo() {
+		return titolo;
+	}
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+	
+	//NUMPAGINE
+	public int getNumPagine() {
+		return numPagine;
+	}
+	public void setNumPagine(int numPagine) {
+		this.numPagine = numPagine;
+	}
+	
+	//AUTORE
+	public String getAutore() {
+		return autore;
+	}
+	public void setAutore(String autore) {
+		this.autore = autore;
+	}
+	
+	//EDITORE
+	public String getEditore() {
+		return editore;
+	}
+	public void setEditore(String editore) {
+		this.editore = editore;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Libro= titolo: " + titolo + " | numero pagine: " + numPagine + " | autore: " + autore + " | editore: " + editore;
+	}
+	
+
+}
--- src/org/lessons/java/books/Main.java
+++ src/org/lessons/java/books/Main.java
@@ -1,8 +1,13 @@
 package org.lessons.java.books;
 
 public class Main {
+	
 	public static void main(String[] args) {
-		System.out.println("hello books");
+		
+//		Prodotto prodotto1 = new Prodotto("telefono", "samsung", 2899, 22);
+		
+		Libro libro1 = new Libro("Triplice furto", 40, "Barbiero Loris", "Youcanprint");
+		System.out.println(libro1);
 	}
 
 }
