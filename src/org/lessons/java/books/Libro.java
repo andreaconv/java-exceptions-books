5435cb69115f4dcc287abe8bb05611e56f83e031
andreaconv <andrea2convertini@gmail.com>
2023-09-19 17:16:17 +0200

    [add] msg d'errore + try catch nel for e stampa array

--- src/org/lessons/java/books/Libro.java
+++ src/org/lessons/java/books/Libro.java
@@ -8,7 +8,7 @@ public class Libro {
 	private String editore;
 	
 	
-	public Libro(String titolo, int numPagine, String autore, String editore) {
+	public Libro(String titolo, int numPagine, String autore, String editore) throws Exception {
 
 		setTitolo(titolo);
 		setNumPagine(numPagine);
@@ -20,7 +20,10 @@ public class Libro {
 	public String getTitolo() {
 		return titolo;
 	}
-	public void setTitolo(String titolo) {
+	public void setTitolo(String titolo) throws Exception {
+		
+		if (titolo.isBlank())
+			throw new Exception("Title can't be empty");
 		this.titolo = titolo;
 	}
 	
@@ -28,7 +31,10 @@ public class Libro {
 	public int getNumPagine() {
 		return numPagine;
 	}
-	public void setNumPagine(int numPagine) {
+	public void setNumPagine(int numPagine) throws Exception {
+		
+		if (numPagine <= 0)
+			throw new Exception("Number of pages can't be 0 or less");
 		this.numPagine = numPagine;
 	}
 	
@@ -36,7 +42,10 @@ public class Libro {
 	public String getAutore() {
 		return autore;
 	}
-	public void setAutore(String autore) {
+	public void setAutore(String autore) throws Exception {
+		
+		if (autore.isBlank())
+			throw new Exception("Author can't be empty");
 		this.autore = autore;
 	}
 	
@@ -44,14 +53,17 @@ public class Libro {
 	public String getEditore() {
 		return editore;
 	}
-	public void setEditore(String editore) {
+	public void setEditore(String editore) throws Exception  {
+		
+		if (editore.isBlank())
+			throw new Exception("Publisher can't be empty");
 		this.editore = editore;
 	}
 	
 
 	@Override
 	public String toString() {
-		return "titolo: " + titolo + " | numero pagine: " + numPagine + " | autore: " + autore + " | editore: " + editore;
+		return "titolo: " + getTitolo() + " | numero pagine: " + getNumPagine() + " | autore: " + getAutore() + " | editore: " + getEditore();
 	}
 	
 
--- src/org/lessons/java/books/Main.java
+++ src/org/lessons/java/books/Main.java
@@ -13,27 +13,46 @@ public class Main {
 		String autore = null;
 		String editore = null;
 		
-		System.out.print("Digitare la quantità di libri che si vogliono inserire ");
-		int LNG = sc.nextInt();
-		sc.nextLine();
+		//LUNGHEZZA
+		System.out.print("Digitare la QUANTITÀ di libri che si vogliono inserire ");
+		
+//		mi salvo il numero inserito
+//		int LNG = sc.nextInt();
+//		sc.nextLine();
+		
+		//ALTERNATIVA che converte una stringa in numero
+		int LNG = Integer.valueOf(sc.nextLine());
 		
 		Libro[] libri = new Libro[LNG];
 		
 		for (int i = 0; i < LNG; i++) {
 			
-			System.out.print("Inserire il titolo: ");
-			titolo = sc.nextLine();
-			System.out.print("Inserire il numero di pagine: ");
-			numPagine = sc.nextInt();
-			sc.nextLine();
-			System.out.print("Inserire l'autore: ");
-			autore = sc.nextLine();
-			System.out.print("Inserire l'editore: ");
-			editore = sc.nextLine();
-			
-			libri[i] = new Libro(titolo, numPagine, autore, editore);
-			
+			try {
+				
+				System.out.print("Inserire il titolo: ");
+				titolo = sc.nextLine();
+				
+				System.out.print("Inserire il numero di pagine: ");
+				numPagine = Integer.valueOf(sc.nextLine());
+				
+				System.out.print("Inserire l'autore: ");
+				autore = sc.nextLine();
+				
+				System.out.print("Inserire l'editore: ");
+				editore = sc.nextLine();
+				
+				libri[i] = new Libro(titolo, numPagine, autore, editore);
+				
+			} catch (Exception e) {
+				
+				System.err.println("Dato inserito errato: " + e.getMessage());
+				
+				i--;
+			}			
 		}
+		sc.close();
+		
+		System.out.println("\n-----------------------------------------\n");
 		
 		for (int i = 0; i < LNG; i++) {
 			
